1d94ea3f0cccd619acc56be0f06530d6a3122df7
Yangolas <yanguasuah@gmail.com>
2023-08-24 20:51:25 +0200

    dsafaesf

--- src/WonderfullOffer.API/Program.cs
+++ src/WonderfullOffer.API/Program.cs
@@ -6,83 +6,85 @@ using WonderfullOffers.Application;
 using WonderfullOffers.Domain;
 using WonderfullOffers.Infraestructure;
 
-Main(new string[] { });
-void Main(string[] args) 
+public partial class Program 
 {
-    var builder = WebApplication.CreateBuilder(args);
-
-    builder.Configuration
-        .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("appsettings.json")
-        .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
-        .Build();
-
-    //This only for using http no https
-    if (builder.Environment.EnvironmentName == "Production")
+    static void Main(string[] args) 
     {
-        builder.WebHost.ConfigureKestrel(options =>
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Configuration
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+            .Build();
+
+        //This only for using http no https
+        if (builder.Environment.EnvironmentName == "Production")
         {
-            options.ListenAnyIP(5000);
-        });
-    }
-
-    builder.Services.AddControllers();
-
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    builder.Services.AddQuartz(q =>
-    {
-        q.UseMicrosoftDependencyInjectionJobFactory();
-
-        var jobKey = new JobKey("ProcessWebCompany");
-        q.AddJob<JobWorkAction>(opts => opts.WithIdentity(jobKey));
-
-        q.AddTrigger(opts => opts
-            .ForJob(jobKey)
-            .WithIdentity("ProcessWebCompany")
-            .WithCronSchedule("* 00 00 ? * *")
-        );
-    });
-    builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
-
-
-    builder.Services.AddServiceApplication();
-    builder.Services.AddServiceDomain();
-    builder.Services.AddServiceInfraestructure(builder.Configuration);
-    builder.Services.AddServicesSettings(builder.Configuration);
-    builder.Services.AddSerilog((ctx, lc) =>
-        lc.MinimumLevel.Debug()
-        .WriteTo.Console()
-        .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
-    );
-
-    builder.Services.AddCors(options =>
-    {
-        options.AddDefaultPolicy(
-            policy =>
+            builder.WebHost.ConfigureKestrel(options =>
             {
-                policy
-                    .AllowAnyOrigin()
-                    .AllowAnyHeader()
-                    .AllowAnyMethod();
+                options.ListenAnyIP(5000);
             });
-    });
+        }
 
-    var app = builder.Build();
-    app.UseCors();
+        builder.Services.AddControllers();
 
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddQuartz(q =>
+        {
+            q.UseMicrosoftDependencyInjectionJobFactory();
+
+            var jobKey = new JobKey("ProcessWebCompany");
+            q.AddJob<JobWorkAction>(opts => opts.WithIdentity(jobKey));
+
+            q.AddTrigger(opts => opts
+                .ForJob(jobKey)
+                .WithIdentity("ProcessWebCompany")
+                .WithCronSchedule("* 00 00 ? * *")
+            );
+        });
+        builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+
+        builder.Services.AddServiceApplication();
+        builder.Services.AddServiceDomain();
+        builder.Services.AddServiceInfraestructure(builder.Configuration);
+        builder.Services.AddServicesSettings(builder.Configuration);
+        builder.Services.AddSerilog((ctx, lc) =>
+            lc.MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+        );
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(
+                policy =>
+                {
+                    policy
+                        .AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+        });
+
+        var app = builder.Build();
+        app.UseCors();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        //app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
-
-    //app.UseHttpsRedirection();
-
-    app.UseAuthorization();
-
-    app.MapControllers();
-
-    app.Run();
 }
