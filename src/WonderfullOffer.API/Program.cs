df8e86f99d60e497a10e7da9868e1928a548753c
Yangolas <yanguasuah@gmail.com>
2023-08-24 21:11:21 +0200

    sadfdsaf

--- src/WonderfullOffer.API/Program.cs
+++ src/WonderfullOffer.API/Program.cs
@@ -8,7 +8,7 @@ using WonderfullOffers.Infraestructure;
 
 public partial class Program 
 {
-    static void Main(string[] args) 
+    public static void Main(string[] args) 
     {
         var builder = WebApplication.CreateBuilder(args);
 
